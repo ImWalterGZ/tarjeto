d8edf763ab8d242ddf0b51ff386b4497882a8819
GZZZ <walter.gz@icloud.com>
2025-01-29 14:58:44 -0600

    Carpeta de imagenes landing, landingComp2

---
+++ frontend/src/components/landing/LandingComp2.jsx
@@ -0,0 +1,3 @@
+export default function LandingComp2() {
+  return <div>Cabrones</div>;
+}
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,4 +1,5 @@
 import HeroSection from "../components/landing/HeroSection";
+import LandingComp2 from "../components/landing/LandingComp2";
 import Navbar from "../components/landing/Navbar";
 
 export default function Landing() {
@@ -6,6 +7,7 @@ export default function Landing() {
     <div>
       <Navbar />
       <HeroSection />;
+      <LandingComp2 />
     </div>
   );
 }
