db5e38c3bf41ecf10857a9afa94147488f1dd6e1
Alex-py004 <ozkarguib04@gmail.com>
2025-02-04 11:42:01 -0600

    Optimización de animacion en las Tarjetas

--- frontend/src/components/landing/LandingComp2.jsx
+++ frontend/src/components/landing/LandingComp2.jsx
@@ -12,30 +12,36 @@ const StepsSection = () => {
       {/* Contenedor de pasos */}
       <div className="flex flex-col md:flex-row justify-center gap-6">
         {/* Tarjeta 1 */}
-        <div className="flex flex-col items-center bg-white border shadow-md rounded-xl p-6 w-80 text-center 
-                        transition-all hover:shadow-xl hover:border-2 hover:border-red-500 hover:shadow-red-300">
-          <PlusCircle size={48} className="text-red-500 mb-4" />
-          <h3 className="text-xl font-bold text-red-500">Paso 1</h3>
-          <p className="font-semibold text-gray-800">Regístrate en la app</p>
-          <p className="text-gray-600 text-sm">Hazlo en un par de minutos y ¡listo!</p>
+        <div className="p-3 bg-gray-100 rounded-xl">
+          <div className="flex flex-col items-center bg-white border shadow-xl rounded-xl p-6 w-80 text-center 
+                          transition-all hover:border-9 hover:border-red-500">
+            <PlusCircle size={48} className="text-red-500 mb-4" />
+            <h3 className="text-xl font-bold text-red-500">Paso 1</h3>
+            <p className="font-semibold text-gray-800">Regístrate en la app</p>
+            <p className="text-gray-600 text-sm">Hazlo en un par de minutos y ¡listo!</p>
+          </div>
         </div>
 
         {/* Tarjeta 2 */}
-        <div className="flex flex-col items-center bg-white border shadow-md rounded-xl p-6 w-80 text-center 
-                        transition-all hover:shadow-xl hover:border-2 hover:border-red-500 hover:shadow-red-300">
-          <MapPin size={48} className="text-red-500 mb-4" />
-          <h3 className="text-xl font-bold text-red-500">Paso 2</h3>
-          <p className="font-semibold text-gray-800">Visita tus lugares favoritos.</p>
-          <p className="text-gray-600 text-sm">No te olvides de registrar tu visita.</p>
+        <div className="p-3 bg-gray-100 rounded-xl">
+          <div className="flex flex-col items-center bg-white border shadow-xl rounded-xl p-6 w-80 text-center 
+                          transition-all hover:border-9 hover:border-red-500">
+            <MapPin size={48} className="text-red-500 mb-4" />
+            <h3 className="text-xl font-bold text-red-500">Paso 2</h3>
+            <p className="font-semibold text-gray-800">Visita tus lugares favoritos</p>
+            <p className="text-gray-600 text-sm">No te olvides de registrar tu visita.</p>
+          </div>
         </div>
 
         {/* Tarjeta 3 */}
-        <div className="flex flex-col items-center bg-white border shadow-md rounded-xl p-6 w-80 text-center 
-                        transition-all hover:shadow-xl hover:border-2 hover:border-red-500 hover:shadow-red-300">
-          <Trophy size={48} className="text-red-500 mb-4" />
-          <h3 className="text-xl font-bold text-red-500">Paso 3</h3>
-          <p className="font-semibold text-gray-800">Gana puntos y disfruta.</p>
-          <p className="text-gray-600 text-sm">Acumula puntos y gana recompensas.</p>
+        <div className="p-3 bg-gray-100 rounded-xl">
+          <div className="flex flex-col items-center bg-white border shadow-md rounded-xl p-6 w-80 text-center 
+                          transition-all hover:border-9 hover:border-red-500">
+            <Trophy size={48} className="text-red-500 mb-4" />
+            <h3 className="text-xl font-bold text-red-500">Paso 3</h3>
+            <p className="font-semibold text-gray-800">Gana puntos y disfruta</p>
+            <p className="text-gray-600 text-sm">Acumula puntos y gana recompensas.</p>
+          </div>
         </div>
       </div>
 
