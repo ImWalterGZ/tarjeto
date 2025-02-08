eda0703e8c9792440940cca71c0dbe6e2a4992e5
GZZZ <walter.gz@icloud.com>
2025-02-08 16:06:02 -0600

    cambio pequeno importacion landing.jsx

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,7 +2,6 @@ import HeroSection from "../components/landing/HeroSection";
 import LandingComp2 from "../components/landing/LandingComp2";
 import Navbar from "../components/landing/Navbar";
 import LandingComp1 from "../components/landing/LandingComp1";
-import LandingComp2 from "../components/landing/LandingComp2";
 
 export default function Landing() {
   return (
