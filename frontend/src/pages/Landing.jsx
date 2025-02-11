276091390756364aed999cffd8718ee3585a1907
Alex-py004 <ozkarguib04@gmail.com>
2025-02-11 02:51:23 -0600

    Subiendo LandingComp4, revisar

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -4,6 +4,7 @@ import Navbar from "../components/landing/Navbar";
 import LandingComp1 from "../components/landing/LandingComp1";
 import LandingComp2 from "../components/landing/LandingComp2";
 import LandingComp3 from "../components/landing/LandingComp3";
+import LandingComp4 from "../components/landing/LandingComp4";
 
 export default function Landing() {
   return (
@@ -13,6 +14,8 @@ export default function Landing() {
       <LandingComp1 />
       <LandingComp2 />
       <LandingComp3 />
+      <LandingComp4 />
+
     </div>
   );
 }
