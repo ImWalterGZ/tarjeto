9e6421e70754a3158e29f670fec5b980c8c39ffc
GZZZ <walter.gz@icloud.com>
2025-03-15 17:29:19 -0600

    Se agrega un header permitido que es Access Control allow credentials, parece que lo arregla

--- server/config/cors.config.js
+++ server/config/cors.config.js
@@ -1,5 +1,11 @@
 export const corsOptions = {
-  origin: ["https://www.tarjeto.app", "https://tarjeto.app"],
+  origin: [
+    "https://www.tarjeto.app",
+    "https://tarjeto.app",
+    "tarjeto.app",
+    "www.tarjeto.app",
+    "http://localhost:5173",
+  ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: [
@@ -9,6 +15,7 @@ export const corsOptions = {
     "Accept",
     "Origin",
     "Cookie",
+    "Access-Control-Allow-Credentials",
   ],
   exposedHeaders: ["set-cookie"],
   optionsSuccessStatus: 200,
